70024817db67248296d5dd9fb8b0dc8b21568cd4
jonnaliesel <jonnaliesel@gmail.com>
2022-05-03 19:07:40 +0200

    Convert shapes from format

--- main.py
+++ main.py
@@ -163,6 +163,25 @@ def create_grid():
 
     return grid
 
+def convert_shape_format(shape):
+    '''
+    Take shape format and create actual shape from reading 0's
+    Returns the coordinates for where the shape will be drawn
+    '''
+    positions = []
+    format = shape.shape[shape.rotation % len(shape.shape)]
+
+    for i, line in enumerate(format):
+        row = list(line)
+        for j, column in enumerate(row):
+            if column == '0':
+                positions.append((shape.x + j, shape.y + i))
+
+    for i, pos in enumerate(positions):
+        positions[i] = (pos[0] - 2, pos[1] - 4)
+
+    return positions
+
 def draw_grid(surface, row, col):
     '''
     Draw line for every row and column in play area
